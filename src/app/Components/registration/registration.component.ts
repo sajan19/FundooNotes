3fae014bdd80021e7c19dbe83c82791aa5a94aed
Sajan <sajanmhatre29@gmail.com>
2021-11-18 23:44:07 +0530

    [Sajan]Add: Registration page added

--- src/app/Components/registration/registration.component.ts
+++ src/app/Components/registration/registration.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
-
-  constructor() { }
+  registerForm: FormGroup;
+  submitted =false;
+  constructor(private formBuilder: FormBuilder) { 
+    this.registerForm = this.formBuilder.group({
+      first_name: ['',[Validators.required, Validators.minLength(3)]],
+      last_name: ['',[Validators.required, Validators.minLength(3)]],
+      // lname: ['',[Validators.required, Validators.minLength(3)]],
+      email: ['',[Validators.required, Validators.email]],
+      password: ['',[Validators.required, Validators.minLength(8)]],
+      cpassword: ['',[Validators.required, Validators.minLength(8)]],
+    });
+  }
+ 
 
   ngOnInit(): void {
   }
+  onSubmit(){
+    console.log(this.registerForm.value);
+    if(this.registerForm.valid){
+      console.log(" Form is Valid. Go to Next Step.");
+    }else{
+      console.log("Invalid Form");
+    }
+}
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './Components/registration/registration.component';
 import { LoginComponent } from './Components/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import {FormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +25,18 @@ import { LoginComponent } from './Components/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSnackBarModule,
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatIconModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
