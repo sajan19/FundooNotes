6f53508bee346ef51597f17129c0e18f3c2a06a3
Sajan <sajanmhatre29@gmail.com>
2021-11-22 23:48:44 +0530

    [Sajan]Add: take notes component added

--- src/app/Components/take-note/take-note.component.ts
+++ src/app/Components/take-note/take-note.component.ts
@@ -11,5 +11,19 @@ export class TakeNoteComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  displayImage: any = true;
+  displayVideo_1: any = false;
+  displayVideo_2: any = false;
+  displayVideo_3: any = false;
+  
+  image1Click(){
+    this.displayImage = false;
+    this.displayVideo_1 = true;
+  }
+  
+  closeVideo(){
+    this.displayImage = true;
+    this.displayVideo_1 = false;
+  }
 
 }
